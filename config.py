383951f725be020b47a1d6b6fc540b69dcbade0d
ifunnyman520 <ifunnyman520@gmail.com>
2021-02-15 16:39:50 +0200

    refactored config

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+import os
 
 
 class Config(object):
-    SECRET_KEY = 'very_secret_key'
-    MONGODB_DB = 'mydb'
-    MONGODB_HOST = 'localhost'
-    MONGODB_PORT = 27017
+    SECRET_KEY = os.getenv('SECRET_KEY', 'very_secret_key')
+    MONGODB_DB = os.getenv('DB_NAME', 'mydb')
+    MONGODB_HOST = os.getenv('DB_HOST', 'localhost')
+    MONGODB_PORT = os.getenv('DB_PORT', 27017)
 
 
 config = Config()
